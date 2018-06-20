cb3b283759e16b70cc6706f2e62893a76f4cc740
daniel.melo <daniefarias.ti@gmail.com>
2018-06-19 23:41:23 -0300

    Relizando a configuração de conexão com o H2, inicialização da entidade de erros desconhecidos, e a internacionalição

--- src/main/java/br/com/liferay/daniel/pointrecord/domain/UnknownError.java
+++ src/main/java/br/com/liferay/daniel/pointrecord/domain/UnknownError.java
@@ -1,4 +1,32 @@
 package br.com.liferay.daniel.pointrecord.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "UNKNOWN_ERROR")
 public class UnknownError {
+
+    @Id
+    private String uuid;
+
+    @Lob
+    @Column
+    private String logger;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getLogger() {
+        return logger;
+    }
+
+    public void setLogger(String logger) {
+        this.logger = logger;
+    }
+
 }
