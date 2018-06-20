cb493da3037e798023b9825c8a7793b99fb8d14e
daniel.melo <daniefarias.ti@gmail.com>
2018-06-20 03:55:49 -0300

    Implmentação da classe de configuração para a autenticação básica em memória

---
+++ src/main/java/br/com/liferay/daniel/pointrecord/config/WebBasicSecurityConfiguration.java
@@ -0,0 +1,51 @@
+package br.com.liferay.daniel.pointrecord.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebBasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .and()
+                .withUser("manager")
+                .password(passwordEncoder().encode("password"))
+                .credentialsExpired(true)
+                .accountExpired(true)
+                .accountLocked(true)
+                .authorities("WRITE_PRIVILEGES", "READ_PRIVILEGES")
+                .roles("MANAGER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/br/com/liferay/daniel/pointrecord/user/UserController.java
+++ src/main/java/br/com/liferay/daniel/pointrecord/user/UserController.java
@@ -1,7 +1,17 @@
 package br.com.liferay.daniel.pointrecord.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/user")
 public class UserController {
+
+    @RequestMapping(value = "/authenticated", method = RequestMethod.GET)
+    public String authenticated (){
+        return "Authenticated";
+    }
+
+
 }
