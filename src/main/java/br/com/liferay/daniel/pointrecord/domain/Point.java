a2b9efe4b4800a807d1b0fa1d580a82046f03fd1
daniel.melo <daniefarias.ti@gmail.com>
2018-06-21 01:05:57 -0300

    Mapeamento das entidades básicas da aplicação com JPA

--- src/main/java/br/com/liferay/daniel/pointrecord/domain/Point.java
+++ src/main/java/br/com/liferay/daniel/pointrecord/domain/Point.java
@@ -1,4 +1,54 @@
 package br.com.liferay.daniel.pointrecord.domain;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "POINT")
 public class Point {
+
+    @GenericGenerator(
+            name = "pointSequenceGenerator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "SEQ_POINT"),
+                    @Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            })
+    @Id
+    @GeneratedValue(generator = "pointSequenceGenerator")
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar dateTime;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Calendar getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Calendar dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/br/com/liferay/daniel/pointrecord/domain/User.java
+++ src/main/java/br/com/liferay/daniel/pointrecord/domain/User.java
@@ -1,4 +1,33 @@
 package br.com.liferay.daniel.pointrecord.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
 public class User {
+
+    @Id
+    private Long pis;
+
+    @Column
+    private String name;
+
+    public Long getPis() {
+        return pis;
+    }
+
+    public void setPis(Long pis) {
+        this.pis = pis;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
